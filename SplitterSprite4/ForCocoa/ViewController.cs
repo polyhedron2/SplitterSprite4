9670bf178510c5909d31c9607c83baa70084b6be
polyhedron2 <polyhedron2@gmail.com>
2019-08-10 03:34:32 +0900

    Mac向けウィンドウ表示

---
+++ SplitterSprite4/ForCocoa/ViewController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using AppKit;
+using Foundation;
+
+namespace ForCocoa
+{
+    public partial class ViewController : NSViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Do any additional setup after loading the view.
+        }
+
+        public override NSObject RepresentedObject
+        {
+            get
+            {
+                return base.RepresentedObject;
+            }
+            set
+            {
+                base.RepresentedObject = value;
+                // Update the view, if already loaded.
+            }
+        }
+
+        public override void ViewWillAppear()
+        {
+            base.ViewWillAppear();
+            string sample = Vanilla.Shared.Text();
+            this.View.Window.Title = sample;
+        }
+    }
+}
